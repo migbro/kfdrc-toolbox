f411902aed0565260a32b983753709054b866990
migbro <miguel.a.brown@gmail.com>
2018-07-17 14:07:54 -0400

    changed output format and created errs files

--- vcf_compare.py
+++ vcf_compare.py
@@ -17,17 +17,18 @@ for i in range(len(vcf_list) - 1):
     for k in range(j, len(vcf_list), 1):
         # sys.stdout.write(vcf_list[i] + ' vs ' + vcf_list[k] + '\n')
         vcf1 = vcf_list[i]
-        vcf1_root = os.path.basename(os.path.splitext(vcf1)[0])
+        vcf1_root = os.path.basename(os.path.splitext(vcf1)[0])[:-4]
         vcf2 = vcf_list[k]
-        vcf2_root = os.path.basename(os.path.splitext(vcf2)[0])
+        vcf2_root = os.path.basename(os.path.splitext(vcf2)[0])[:-4]
+        super_root = + vcf1_root + '_vs_' + vcf2_root
         cmd = bedtools + ' intersect -a ' + vcf1 + ' -b ' + vcf2 \
-              + ' -v -sorted > ' + vcf1_root + '_vs_' + vcf2_root + '.vcf'
+              + ' -v -sorted > ' + super_root + '.vcf 2> ' + super_root + '.errs'
         sys.stderr.write(cmd + '\n')
         subprocess.Popen(cmd, shell=True, stdin=None, stdout=None,
                          stderr=None, close_fds=True)
-
+        super_root = vcf2_root + '_vs_' + vcf1_root
         cmd = bedtools + ' intersect -a ' + vcf2 + ' -b ' + vcf1 \
-              + ' -v -sorted > ' + vcf2_root + '_vs_' + vcf1_root + '.vcf'
+              + ' -v -sorted > ' + super_root + '.vcf 2> ' + super_root + '.errs'
         sys.stderr.write(cmd + '\n')
         subprocess.Popen(cmd, shell=True, stdin=None, stdout=None,
                          stderr=None, close_fds=True)
