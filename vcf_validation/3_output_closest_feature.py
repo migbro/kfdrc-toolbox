0704499464a80203b42881030053e6bebaf9fc48
migbro <miguel.a.brown@gmail.com>
2018-07-18 10:46:19 -0400

    Reorganized some scripts, created mini-pipe to get next closest values, sorted by distance and showing only positions and ref/alt seqs

---
+++ vcf_validation/3_output_closest_feature.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# use a-not-in-b bed files from vcf compare to look for closest features in b for near-matched hits
+
+import sys
+import os
+import subprocess
+
+bedtools = sys.argv[1]
+comp_list = sys.argv[2]
+passed_list = sys.argv[3]
+
+# tools = ['VARDICT', 'STRELKA', 'MUTECT2', 'LANCET']
+
+for fn in open(comp_list):
+    fn = fn.rstrip('\n')
+    fn_half = os.path.basename(fn).split('_vs_')
+    fn_parts_a = fn_half[0].split('_')
+    # fn_parts_b = fn_half[1].split('_')
+    for fn2 in open(passed_list):
+        fn2 = fn2.rstrip('\n')
+        fn2_root = os.path.basename(fn2).split('.')[0]
+        if fn2_root != fn_parts_a:
+            cmd = bedtools + ' closest -a ' + fn + ' -b ' \
+                  + fn2 + ' -d | sort -nk25 l_v_m_closest.bed | cut -f 1-7,13-19,25 > ' + fn_parts_a + '_' + fn2_root \
+                  + '.closest.bed'
+            subprocess.call(cmd, shell=True)
+
+
