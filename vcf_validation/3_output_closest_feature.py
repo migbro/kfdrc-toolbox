890dd24f9b9f7f40c0c1cc2330cd3dda93c5bb7b
migbro <miguel.a.brown@gmail.com>
2018-07-18 13:34:29 -0400

    after Bo took a look, discovered script was making extra and wrong comparisons.  Should ensure correct comparisones are made

--- vcf_validation/3_output_closest_feature.py
+++ vcf_validation/3_output_closest_feature.py
@@ -10,19 +10,27 @@ comp_list = sys.argv[2]
 passed_list = sys.argv[3]
 
 # tools = ['VARDICT', 'STRELKA', 'MUTECT2', 'LANCET']
+passed_attr = {}
+for fn in open(passed_list):
+    fn = fn.rstrip('\n')
+    fn_parts = os.path.basename(fn).split('.')
+    (fid, tool) = fn_parts[0].split('_')
+    passed_attr[tool] = {}
+    passed_attr[tool]['fn_root'] = fn_parts[0]
+    passed_attr[tool]['path'] = fn
+
 
 for fn in open(comp_list):
     fn = fn.rstrip('\n')
     fn_half = os.path.basename(fn).split('_vs_')
-    # fn_parts_a = fn_half[0].split('_')
-    # fn_parts_b = fn_half[1].split('_')
-    for fn2 in open(passed_list):
-        fn2 = fn2.rstrip('\n')
-        fn2_root = os.path.basename(fn2).split('.')
-        if fn2_root[0] != fn_half[0]:
-            cmd = bedtools + ' closest -a ' + fn + ' -b ' \
-                  + fn2 + ' -d | sort -nk25 | cut -f 1-7,13-19,25 > ' + fn_half[0] + '_' + fn2_root[0] + '.closest.bed'
-            sys.stderr.write(cmd + '\n')
-            subprocess.call(cmd, shell=True)
+    fn_parts_a = fn_half[0].split('_')
+    fn_parts_b = fn_half[1].split('_')
+    tool = fn_parts_b[1]
+    tool = tool.replace('.bed', '')
+    cmd = bedtools + ' closest -a ' + fn + ' -b ' + passed_attr[tool]['path'] \
+          + ' -d | sort -nk25 | cut -f 1-7,13-19,25 > ' + fn_half[0] + '_' + passed_attr[tool]['fn_root'] \
+          + '.closest.bed'
+    sys.stderr.write(cmd + '\n')
+    subprocess.call(cmd, shell=True)
 
 
