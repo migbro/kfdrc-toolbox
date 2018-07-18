6995cafcbd11b9cd59c909ec86cd076676cafce6
migbro <miguel.a.brown@gmail.com>
2018-07-18 10:56:14 -0400

    troubleshoot bug in filename parsing

--- vcf_validation/3_output_closest_feature.py
+++ vcf_validation/3_output_closest_feature.py
@@ -14,14 +14,14 @@ passed_list = sys.argv[3]
 for fn in open(comp_list):
     fn = fn.rstrip('\n')
     fn_half = os.path.basename(fn).split('_vs_')
-    fn_parts_a = fn_half[0].split('_')
+    # fn_parts_a = fn_half[0].split('_')
     # fn_parts_b = fn_half[1].split('_')
     for fn2 in open(passed_list):
         fn2 = fn2.rstrip('\n')
         fn2_root = os.path.basename(fn2).split('.')
-        if fn2_root[0] != fn_parts_a:
+        if fn2_root[0] != fn_half[0]:
             cmd = bedtools + ' closest -a ' + fn + ' -b ' \
-                  + fn2 + ' -d | sort -nk25 l_v_m_closest.bed | cut -f 1-7,13-19,25 > ' + fn_parts_a + '_' \
+                  + fn2 + ' -d | sort -nk25 l_v_m_closest.bed | cut -f 1-7,13-19,25 > ' + fn_half[0] + '_' \
                   + fn2_root[0] + '.closest.bed'
             subprocess.call(cmd, shell=True)
 
