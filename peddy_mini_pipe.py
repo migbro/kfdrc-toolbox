d047968de064a891cf934f1b7a0584ac442199a1
migbro <miguel.a.brown@gmail.com>
2018-07-26 14:13:55 -0400

    Added a mini-pipe to test peddy output before and after genotpye refinement

---
+++ peddy_mini_pipe.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import subprocess
+
+peddy = sys.argv[1]
+
+for line in open(sys.argv[2]):
+    (pre_vcf, fam, post_id) = line.rstrip().split()
+    pre_id = pre_vcf.split('.')[0]
+    ped = fam + '.ped'
+    post_vcf = post_id + '.postCGP.Gfiltered.deNovos.vcf.gz'
+    os.mkdir(fam)
+    os.chdir(fam)
+    cmd = 'python -m ' + peddy + ' -p 4 --sites hg38 --prefix ' + fam + '_pre --plot ../' + pre_vcf + ' ../' + ped \
+          + ' && python -m ' + peddy + ' -p 4 --sites hg38 --prefix ' + fam + '_post --plot ../' + post_vcf + ' ../' \
+          + ped
+    sys.stderr.write('Running peddy ' + cmd + '\n')
+    subprocess.call(cmd, shell=True)
