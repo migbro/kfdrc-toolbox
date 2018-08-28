c45d5fc5e94328c078b08a339a397d274f74eb3a
migbro <miguel.a.brown@gmail.com>
2018-08-28 14:01:19 -0400

    added maf filter script

---
+++ filter_mafs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+blacklist = {}
+for entry in open(sys.argv[1]):
+    blacklist[entry.rstrip('\n')] = 0
+
+maf_list = open(sys.argv[2])
+fcol = int(sys.argv[3])
+for maf in maf_list:
+    maf = maf.rstrip('\n')
+    cur = open(maf)
+    new = open(os.path.basename(maf)[-4:] + 'filtered.maf', 'w')
+    head = next(cur)
+    new.write(head)
+    head = next(cur)
+    new.write(head)
+    for entry in cur:
+        info = entry.rstrip('\n').split('\t')
+        if info[fcol] not in blacklist:
+            new.write(entry)
+    cur.close()
+    new.close()
+
+maf_list.close()
