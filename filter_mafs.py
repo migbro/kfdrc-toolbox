42291b1c5343db824f6d5051ea1367c65d9fcf94
migbro <miguel.a.brown@gmail.com>
2018-08-28 14:17:31 -0400

    fixed output file name

--- filter_mafs.py
+++ filter_mafs.py
@@ -12,7 +12,7 @@ fcol = int(sys.argv[3])
 for maf in maf_list:
     maf = maf.rstrip('\n')
     cur = open(maf)
-    new_fn = os.path.basename(maf)[-4:] + 'filtered.maf'
+    new_fn = os.path.basename(maf)[:-4] + '.filtered.maf'
     new = open(new_fn, 'w')
     head = next(cur)
     new.write(head)
