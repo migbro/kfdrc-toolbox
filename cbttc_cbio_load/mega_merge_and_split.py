8bba59865ca52cec6fd3407a8bf9110f5f490e5e
migbro <miguel.a.brown@gmail.com>
2018-09-07 17:30:20 -0400

    switched to using file with header for mafs - was screwy when using a string

--- cbttc_cbio_load/mega_merge_and_split.py
+++ cbttc_cbio_load/mega_merge_and_split.py
@@ -41,24 +41,10 @@ maf_dir = sys.argv[5]
 mega_sample_sheet = open(sys.argv[6])
 mega_patient_sheet = open(sys.argv[7])
 skip_data = sys.argv[8]
-header = '#version 2.4\nHugo_Symbol\tEntrez_Gene_Id\tCenter\tChromosome\tStart_Position\tEnd_Position\tStrand' \
-         '\tVariant_Classification\tVariant_Type\tReference_Allele\tTumor_Seq_Allele1\tTumor_Seq_Allele2\tdbSNP_RS' \
-         '\tdbSNP_Val_Status\tTumor_Sample_Barcode\tMatched_Norm_Sample_Barcode\tMatch_Norm_Seq_Allele1' \
-         '\tMatch_Norm_Seq_Allele2\tTumor_Validation_Allele1\tTumor_Validation_Allele2\tMatch_Norm_Validation_Allele1' \
-         '\tMatch_Norm_Validation_Allele2\tVerification_Status\tValidation_Status\tMutation_Status\tSequencing_Phase' \
-         '\tSequence_Source\tValidation_Method\tScore\tBAM_File\tSequencer\tTumor_Sample_UUID' \
-         '\tMatched_Norm_Sample_UUID\tHGVSc\tHGVSp\tHGVSp_ShortTranscript_ID\tExon_Number\tt_depth\tt_ref_count' \
-         '\tt_alt_count\tn_depth\tn_ref_count\tn_alt_count\tall_effects\tAllele\tGene\tFeature\tFeature_type' \
-         '\tConsequencecDNA_position\tCDS_position\tProtein_position\tAmino_acids\tCodons\tExisting_variation' \
-         '\tALLELE_NUM\tDISTANCE\tSTRAND_VEP\tSYMBOL\tSYMBOL_SOURCE\tHGNC_ID\tBIOTYPE\tCANONICAL\tCCDS\tENSP' \
-         '\tSWISSPROT\tTREMBL\tUNIPARC\tRefSeq\tSIFT\tPolyPhen\tEXON\tINTRON\tDOMAINS\tAF\tAFR_AF\tAMR_AF\tASN_AF' \
-         '\tEAS_AF\tEUR_AF\tSAS_AF\tAA_AF\tEA_AF\tCLIN_SIG\tSOMATIC\tPUBMED\tMOTIF_NAME\tMOTIF_POS\tHIGH_INF_POS' \
-         '\tMOTIF_SCORE_CHANGE\tIMPACT\tPICK\tVARIANT_CLASS\tTSL\tHGVS_OFFSETPHENO\tMINIMISED\tExAC_AF\tExAC_AF_AFR' \
-         '\tExAC_AF_AMR\tExAC_AF_EAS\tExAC_AF_FIN\tExAC_AF_NFE\tExAC_AF_OTH\tExAC_AF_SAS\tGENE_PHENO\tFILTER' \
-         '\tflanking_bps\tvariant_id\tvariant_qual\tExAC_AF_Adj\tExAC_AC_AN_Adj\tExAC_AC_AN\tExAC_AC_AN_AFR' \
-         '\tExAC_AC_AN_AMR\tExAC_AC_AN_EAS\tExAC_AC_AN_FIN\tExAC_AC_AN_NFE\tExAC_AC_AN_OTH\tExAC_AC_AN_SAS' \
-         '\tExAC_FILTER\tgnomAD_AF\tgnomAD_AFR_AF\tgnomAD_AMR_AF\tgnomAD_ASJ_AF\tgnomAD_EAS_AF\tgnomAD_FIN_AF' \
-         '\tgnomAD_NFE_AF\tgnomAD_OTH_AF\tgnomAD_SAS_AF\n'
+
+with open(sys.argv[9]) as f:
+    header = f.read()
+
 dx_dict = {}
 maf_fh = {}
 cnv_fh = {}
