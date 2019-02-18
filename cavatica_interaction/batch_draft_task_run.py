d4ba511ec9369fd90caf89cbfb67f7b731fb4068
migbro <miguel.a.brown@gmail.com>
2019-02-18 14:31:33 -0500

    :wrench: add newline for log output

--- cavatica_interaction/batch_draft_task_run.py
+++ cavatica_interaction/batch_draft_task_run.py
@@ -47,5 +47,5 @@ else:
         stop = len(draft_tasks)
     for i in range(cur_run, stop):
         draft_tasks[i].run()
-        out_fh.write('Running task ' + draft_tasks[i].id + ' ' + draft_tasks[i].name)
+        out_fh.write('Running task ' + draft_tasks[i].id + ' ' + draft_tasks[i].name + '\n')
 out_fh.close()
