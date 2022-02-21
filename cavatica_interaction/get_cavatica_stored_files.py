d89c0761215e1f8d8a4ae113f906fbdd95f7535f
migbro <miguel.a.brown@gmail.com>
2022-02-21 14:48:21 -0500

    :tada: Used and Mod Bo's code to get list of all non-bucket files ina project

---
+++ cavatica_interaction/get_cavatica_stored_files.py
@@ -0,0 +1,72 @@
+"""
+Quick script from Bo Zhang and modifed by Miguel Brown to get files and their sizes from
+cavatica projects that are not in external buckets. Useful for estimating
+cost of storage hits against billing group for a project
+"""
+import sevenbridges as sbg
+from sevenbridges.errors import SbgError
+from sevenbridges.http.error_handlers import rate_limit_sleeper, maintenance_sleeper
+import argparse
+import sys
+import pdb
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--project", required=True)
+parser.add_argument("--profile", required=True)
+args = parser.parse_args()
+
+c = sbg.Config(profile=args.profile)
+api = sbg.Api(config=c, error_handlers=[rate_limit_sleeper, maintenance_sleeper])
+
+
+def iterate_folder(folder_obj):
+    """
+    Folders are funky in this api - there is no .all(). Need to iterate if more than 50 files in folder
+    """
+    # Need this array as a proper bookmark of sorts to ensure you make it to the next page
+    sys.stderr.write('Found dir ' + folder_obj.name + ', drilling down\n')
+    folder_list = []
+    j = 0
+    folder_list.append(folder_obj.list_files())
+    total = folder_obj.list_files().total
+    i = len(folder_list[j])
+    for obj in folder_list[j]:
+        if obj.is_folder():
+            iterate_folder(obj)
+        else:
+            check_file(obj)
+    while i < (total - 1):
+        folder_list.append(folder_list[j].next_page())
+        j += 1
+        for obj in folder_list[j]:
+            if obj.is_folder():
+                iterate_folder(obj)
+            else:
+                check_file(obj)
+        i += len(folder_list[j])
+
+    for obj in folder_obj.list_files():
+        i += 1
+        if obj.is_folder():
+            iterate_folder(obj)
+        else:
+            check_file(obj)
+
+
+def check_file(file_obj):
+    if file_obj.storage.volume is None:
+        print(file_obj.id + "\t" + file_obj.name + "\t" + str(file_obj.size) + "\t" + str(file_obj.size/1073741824))
+
+
+files = api.files.query(project=args.project).all()
+print("File ID\tFile Name\tFile Size bytes\tFile Size GB")
+for f in files:
+    try:
+        if f.is_folder():
+            iterate_folder(f)
+        else:
+            check_file(f)
+    except Exception as e:
+        sys.stderr.write("Ran into error processing " + f.name + " with error: " + str(e) + "\n")
+        exit(1)
