4c9a2b655a8eea5f881611072763a3e23bd6bd5c
Miguel Brown <miguel.a.brown@gmail.com>
2025-09-04 13:14:34 -0400

    :hammer: improve updates

--- cavatica_interaction/download_cavatica_dir.py
+++ cavatica_interaction/download_cavatica_dir.py
@@ -1,33 +1,51 @@
-import sevenbridges as sbg
+"""Recursively download a directory from Cavatica to a local path."""
 import argparse
-from sevenbridges.http.error_handlers import rate_limit_sleeper, maintenance_sleeper
+import sys
 from pathlib import Path
-import pdb
+from time import sleep
+
+import sevenbridges as sbg
+from sevenbridges.http.error_handlers import maintenance_sleeper, rate_limit_sleeper
 
 
+def recursive_download(api: sbg.Api, file_id: str, path: Path) -> None:
+    """Recursively download a directory from Cavatica to a local path.
 
-parser = argparse.ArgumentParser(description='Run a subset of drafted tasks at all times.'
-                                             'Use in conjunction with cron tab')
-parser.add_argument('-p', '--profile', action='store', dest='profile', help='cavatica profile name')
-parser.add_argument('-f', '--folder_id', action='store', dest='folder_id', help='ID of folder to recursively download from')
+    Will make local directories as needed, and download files to match the tree structure
 
+    Args:
+        api (sbg.Api): An authenticated Cavatica API object
+        file_id (str): The ID of the file or folder to download
+        path (Path): The local path to download to, based on the remote structure
 
-def recursive_download(api, file_id, path: Path):
+    """
     file_obj = api.files.get(id=file_id)
     if file_obj.is_folder():
         path = path / Path(file_obj.name)
         path.mkdir(parents=True, exist_ok=True)
-        print(f"Downloading from dir {file_obj.name}")
-        for item in api.files.query(parent=file_obj):
-            print(f"Recursively downloading {item.name}")
+        print(f"Downloading from dir {path}", file=sys.stderr)
+        for item in api.files.query(parent=file_obj).all():
             recursive_download(api, item.id, path)
     else:
-        file_obj.download(path=str(path/file_obj.name))
+        print(f"Downloading {file_obj.name}", file=sys.stderr)
+        dl = file_obj.download(path=str(path / file_obj.name), wait=False)
+        dl.start()
+        while dl.status != "COMPLETED":
+            print(f"  {dl.status}, {dl.progress} completed, {dl.duration} elapsed", file=sys.stderr)
+            sleep(1)
+        print(f"{dl.status}", file=sys.stderr)
 
+parser = argparse.ArgumentParser(
+    description="Run a subset of drafted tasks at all times.Use in conjunction with cron tab"
+)
+parser.add_argument("-p", "--profile", action="store", dest="profile", help="cavatica profile name")
+parser.add_argument(
+    "-f", "--folder_id", action="store", dest="folder_id",
+    help="ID of folder to recursively download from",
+)
 
 args = parser.parse_args()
 
 config = sbg.Config(profile=args.profile)
 api = sbg.Api(config=config, error_handlers=[rate_limit_sleeper, maintenance_sleeper])
-recursive_download(api, args.folder_id, Path('./'))
-
+recursive_download(api, args.folder_id, Path("./"))
